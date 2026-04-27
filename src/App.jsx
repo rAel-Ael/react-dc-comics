de8a1d3a3feee3be9910ec6201f81e526cb785a0
rAel-Ael <alediru9267@gmail.com>
2026-04-27 15:48:14 +0200

    messo un logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
+import Header from "./components/Header"
 function App() {
-  return
-  
+  return <Header/>
   
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+    return <img src="/img/dc-logo.png" alt="" />
+    
+}
+export default Header;
