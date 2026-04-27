5abaf3b57356b9796c3ca63552680b8ff91f69fd
rAel-Ael <alediru9267@gmail.com>
2026-04-27 19:42:33 +0200

    aggiunto primo footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import Content from "./components/Content";
 import Header from "./components/Header"
 import LinksHeader from "./components/LinksHeader"
 import ProductZone from "./components/ProductZone";
+import Footer0 from "./components/Footer0";
 function App() {
   return (
     <>
       <Header />
       <LinksHeader />
       <Content />
-      <ProductZone/>
+      <ProductZone />
+      <><Footer0></Footer0></>
     </>
   );
 }
---
+++ src/components/Footer0.jsx
@@ -0,0 +1,52 @@
+function Footer0() {
+    return (
+        <>
+            <div>
+                <h4>DC Comics</h4>
+                <ul>
+                    <li><a href="">Charater</a></li>
+                    <li><a href="">Comics</a></li>
+                    <li><a href="">Movies</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">Games</a></li>
+                    <li><a href="">News</a></li>
+                    <li><a href="">Videos</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>SHOP</h4>
+                <ul>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>DC</h4>
+                <ul>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                </ul>
+            </div>
+<div>
+                <h4>SITES</h4>
+                <ul>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                    <li><a href="">dc</a></li>
+                </ul>
+            </div>
+        </>
+    )
+}
+export default Footer0;
